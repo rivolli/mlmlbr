6540943b69f8b21ae03d7a8ff928917360f9eec3
Adriano Rivolli <rivolli@gmail.com>
2015-06-29 08:56:29 -0300

    Generating meta

---
+++ generatemeta.R
@@ -0,0 +1,41 @@
+# R Code
+# 12/05/2015
+# A. Rivolli, A. C.P.L.F. Carvalho, 2015
+# Start the experiment for all datasets
+
+setup = function() {
+  files = list.files("exp/", recursive=TRUE, full.name=TRUE);
+  for(i in files) source(i);
+}
+
+generate_meta = function() {
+  setup();
+  
+  #Generate Metabase default
+  metabase <- dataset <- do.call("rbind", lapply(FILES, function(file) {
+    path <- get_filenames(file)
+    alldata <- read.csv.file(path$get_tempfile('onlyfeatures', '.csv'))
+    rownames(alldata) <- paste(path$datasetname, change_special_chars(rownames(alldata)), sep='_')
+    alldata
+  }));
+  write.csv(metabase, file='0metabase.csv')
+  
+  #Generate targets results
+  lapply(FILES, function(file) {
+    path <- get_filenames(file)
+    load(path$get_rdatafile('details')) #results
+    
+#     labelnames <- change_special_chars(rownames(read.csv.file(path$resultfile)))
+#     nresults <- lapply(results, function (item) {
+#       N <- 100
+#       for (i in 1:10) {
+#         item$BASELINE[i, "Accuracy"] * N
+#       }
+#     })
+#     
+    browser()
+  })
+  
+  cat("\ndone:", now(), "\n")
+  TRUE
+}
